0c5350df760359f57214beddfebf0bd553934ba3
TonyWang <2894735011@qq.com>
2022-09-19 23:35:34 +0800

    fix branch merge conflict (InsPoint.cs)

--- Assets/Scripts/InsPoint.cs
+++ Assets/Scripts/InsPoint.cs
@@ -8,39 +8,50 @@ public class InsPoint : MonoBehaviour
     public GameObject PrePoint;
     public float MinDis = 3f;
     public float MaxDis = 50f;
+    public static int x = 0;
 
     private Vector3 v3Ava;
+
+    public static InsPoint Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // StartCoroutine("InsPointFuc1");
+        StartCoroutine("InsPointFuc1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator InsPointFuc1()
     {
-
         while (true)
         {
-            InsPointFuc();
-            InsPointFuc();
-            InsPointFuc();
-            InsPointFuc();
-            InsPointFuc();
-            InsPointFuc();
+            if (x <= 16)
+            {
+                InsPointFuc();
+                x++;
+                InsPointFuc();
+                x++;
+                InsPointFuc();
+                x++;
+                InsPointFuc();
+                x++;
+            }
             print("DoSomething Loop");
 
-            yield return new WaitForSeconds(3);
-
+            yield return new WaitForSeconds(1);
+            Debug.Log("existing objects" + x);
         }
-
     }
 
     public void InsPointFuc()
@@ -52,5 +63,10 @@ public class InsPoint : MonoBehaviour
         Vector3 _v3Point = new Vector3(v3Ava.x + _pNor.x * _dis, 0, v3Ava.z + _pNor.y * _dis);
         GameObject _poiMark = Instantiate(PrePoint, _v3Point, transform.rotation);
 
+    }
+
+    public void deletePoint()
+    {
+        x--;
     }
 }
