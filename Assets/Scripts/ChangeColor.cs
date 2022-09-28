601f69064f909e9e9e0e449bb47ea153ee26e55b
alfred0303 <alfredlee0731@gmail.com>
2022-09-28 21:49:51 +0800

    Add glove prefabs and glove effect

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material[] material;
+    public int x;
+    Renderer read;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0;
+        read = GetComponent<Renderer>();
+        read.enabled = true;
+
+        read.sharedMaterial = material[x];
+        
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // read.sharedMaterial = material[x];
+        
+    }
+
+    public void ChangeToClassA(){
+        
+        read.sharedMaterial = material[2];
+       
+    }
+
+     public void ChangeToClassB(){
+        
+        read.sharedMaterial = material[1];
+       
+    }
+     public void ChangeToClassC(){
+        
+        read.sharedMaterial = material[0];
+       
+    }
+}
